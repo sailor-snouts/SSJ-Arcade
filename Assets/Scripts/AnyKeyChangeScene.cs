70aec99843dd9b87d916c9459175887acbe73ea4
Robert Parker <rparker@kodama.design>
2019-06-19 19:08:25 -0700

    escape on quite, title to main with fade on any

---
+++ Assets/Scripts/AnyKeyChangeScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AnyKeyChangeScene : MonoBehaviour
+{
+    private SceneChange sceneChange;
+    private AudioSource audio;
+
+    [SerializeField]
+    private string nextScene = "Main";
+    [SerializeField]
+    private AudioClip selectSFX;
+
+    private void Start()
+    {
+        this.sceneChange = FindObjectOfType<SceneChange>();
+        this.audio = FindObjectOfType<AudioSource>();
+    }
+
+    void Update()
+    {
+        if(!Input.GetKey(KeyCode.Escape) && Input.anyKeyDown)
+        {
+            this.audio.PlayOneShot(this.selectSFX);
+            this.sceneChange.FadeToLevel(this.nextScene);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Animator))]
+public class SceneChange : MonoBehaviour
+{
+    private Animator animator;
+    private string nextLevel;
+
+    private void Start()
+    {
+        this.animator = GetComponent<Animator>();
+    }
+
+    public void FadeToLevel(string level)
+    {
+        this.animator.SetTrigger("FadeOut");
+        this.nextLevel = level;
+    }
+
+    public void OnComplete()
+    {
+        SceneManager.LoadScene(this.nextLevel);
+    }
+}
