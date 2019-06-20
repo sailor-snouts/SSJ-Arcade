1af1a5bb645bf3644d4aae892e18a5a4bbe359fa
Robert Parker <rparker@kodama.design>
2019-06-19 19:52:47 -0700

    rewired controls (qwe,asd) or joystick buttons 1-3 top and bottom row

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class PlayerController : MonoBehaviour
+{
+    private int score;
+    private int combo;
+
+    private int playerId = 0;
+    private Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.player = ReInput.players.GetPlayer(this.playerId);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.player.GetButtonDown("Red"))
+        {
+            Debug.Log(this.player + "Pressed Red");
+        }
+        if (this.player.GetButtonDown("Blue"))
+        {
+            Debug.Log(this.player + "Pressed Blue");
+        }
+        if (this.player.GetButtonDown("Green"))
+        {
+            Debug.Log(this.player + "Pressed Green");
+        }
+        if (this.player.GetButtonDown("Orange"))
+        {
+            Debug.Log(this.player + "Pressed Orange");
+        }
+        if (this.player.GetButtonDown("Pink"))
+        {
+            Debug.Log(this.player + "Pressed Pink");
+        }
+        if (this.player.GetButtonDown("Black"))
+        {
+            Debug.Log(this.player + "Pressed Black");
+        }
+    }
+}
