a962740c5a7873ee5d378951436924c5feb7bf70
sirslade <sirslade@gmail.com>
2019-06-24 22:09:08 -0700

    Add HighScore script, scores json file

---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class HighScore : MonoBehaviour
+{
+    [SerializeField]
+    TextAsset scoreFile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] jsonObjects = scoreFile.text.Split('\n');
+        List<Score> scores = new List<Score>();
+        for(int i = 0 ; i < jsonObjects.Length ; i++)
+        {
+            Debug.Log(jsonObjects[i]);
+            Score s = JsonUtility.FromJson<Score>(jsonObjects[i]);
+            scores.Add(s);
+        }
+        foreach(Score s in scores){
+        Debug.Log(s.initials);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[Serializable]
+public class Score
+{
+    public string score;
+    public string initials;
+}
